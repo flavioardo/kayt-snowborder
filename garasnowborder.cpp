81006ed464b1552aeeacd255175712836040a487
flavioardo <122883504+flavioardo@users.noreply.github.com>
2023-02-20 16:18:30 +0100

    .
    .

---
+++ garasnowborder.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int scelta;
+        do
+        {
+            cout<<endl<<"MENU'"<<endl;
+            cout<<"1) Genera gara"<<endl;
+            cout<<"2) Visualizza risultato"<<endl;
+            cout<<"3)USCITA "<<endl;
+            cout<<"Inserire una scelta: "<<endl;
+            cin>>scelta;
+            switch(scelta){
+                case 1:
+                    
+                        break;
+                    case 2:
+                        
+                        
+                        break;
+            }
+
+        }while(scelta!=3);
+        
+
+    return 0;
+}
+
