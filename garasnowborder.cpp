1de42bcb6fc42895042797233d3f6c5ccb0c3b2d
flavioardo <122883504+flavioardo@users.noreply.github.com>
2023-02-21 18:00:32 +0100

    Update garasnowborder.cpp

--- garasnowborder.cpp
+++ garasnowborder.cpp
@@ -1,14 +1,49 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+srand(time(NULL));
+
+struct s_snow
+{
+    int matricola;
+    string cognome;
+    int x;
+    int y;
+}
+
+int genera()
+{
+    int x1=0;
+    int y1=0;
+    
+    for(int i=0; i<30; i++)
+    {
+        x=rand()%100;
+        y=rand()%100;
+        calcola(x,y);
+        dist1=sqrt((pow(x,2)-pow(x1,2))+(pow(y,2)-pow(y1,2)))
+        x1=x;
+        y1=y;
+        dist=dist+dist1;
+    }
+}
+
+void gara()
+{
+    string scelta="";
+    int c=1;
+    
+    do{
+    
+        cout<<"Inserire cognome partecipante: ";
+        //cin>>cognome;
+        cout<<"Vuole inserire un altro partecipante(si/no)";
+        //cin>>scelta;
+        
+    
+    }while(scelta!="no");
+}
+
 
 int main()
 {
@@ -24,7 +59,7 @@ int main()
             cin>>scelta;
             switch(scelta){
                 case 1:
-                    
+                        gara();
                         break;
                     case 2:
                         
